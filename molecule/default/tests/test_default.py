56f81ac8f8ec22a22787863b9da5285ae942b15d
darkwizard242 <am900820@gmail.com>
2020-11-24 19:32:10 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,14 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE_BINARY = "/usr/local/bin/docker-compose"
+
+
 def test_dockercompose_binary_exists(host):
-    assert host.file('/usr/local/bin/docker-compose').exists
+    """
+    Tests if docker-compose binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_dockercompose_binary_file(host):
-    assert host.file('/usr/local/bin/docker-compose').is_file
+    """
+    Tests if docker-compose binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_dockercompose_binary_which(host):
-    assert host.check_output('which docker-compose') == \
-      '/usr/local/bin/docker-compose'
+    """
+    Tests the output to confirm docker-compose's binary location.
+    """
+    assert host.check_output('which docker-compose') == PACKAGE_BINARY
